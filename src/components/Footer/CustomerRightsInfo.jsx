43e508ca69ab5f1159f9d4b1bb107b6e203e517e
Eone-tgx <eone1024@gmail.com>
2025-07-26 09:17:08 +0300

    footer-section

---
+++ src/components/Footer/CustomerRightsInfo.jsx
@@ -0,0 +1,9 @@
+import css from "./CustomerRightsInfo.module.css";
+
+function CustomerRightsInfo() {
+  return (
+    <div className={css.rightsInfo}>© 2025 Harmoniq. All rights reserved.</div>
+  );
+}
+
+export default CustomerRightsInfo;
