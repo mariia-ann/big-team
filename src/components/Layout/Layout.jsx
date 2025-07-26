24033fd3ef992685ea7645f65bd1c0265e5a4a89
Mariia <myshyck@gmail.com>
2025-07-27 00:02:49 +0300

    fix: update package.json, add settings for layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import Footer from "../Footer/Footer.jsx";
 import Header from "../Header/Header.jsx";
+import style from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className={style.layout}>
       <Header />
-      <main>{children}</main>
+      <main className={style.main}>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
