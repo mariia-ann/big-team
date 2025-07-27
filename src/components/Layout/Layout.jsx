431fa945864d2201398d06a318e24db9432a9959
Mariia <myshyck@gmail.com>
2025-07-27 12:03:53 +0300

    fix: hover modal

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,10 @@
 import Footer from "../Footer/Footer.jsx";
 import Header from "../Header/Header.jsx";
-import AuthModal from "../ModalErrorSave/ModalErrorSave.jsx";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <AuthModal />
       <main>{children}</main>
       <Footer />
     </>
