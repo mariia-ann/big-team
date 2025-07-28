a6befdffaecd964592f873b162ecef4b52a242ca
Maryna <tromsa3122@mail.com>
2025-07-28 14:04:07 +0300

    add link and fix

--- src/components/CreatorsSection/CreatorsSection.jsx
+++ src/components/CreatorsSection/CreatorsSection.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import styles from "./CreatorsSection.module.css";
-import arrowIcon from "../../assets/images/icons/arrow.svg";
-import Container from "../Container/Container";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CreatorsSection.module.css';
+import arrowIcon from '../../assets/images/icons/arrow.svg';
+import Container from '../Container/Container';
 
 const CreatorsSection = () => {
   const [creators, setCreators] = useState([]);
@@ -10,11 +10,11 @@ const CreatorsSection = () => {
   useEffect(() => {
     const fetchCreators = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/creators");
+        const res = await fetch('http://localhost:3000/api/creators/top-creators');
         const data = await res.json();
         setCreators(data);
       } catch (error) {
-        console.error("Failed to fetch creators:", error);
+        console.error('Failed to fetch creators:', error);
       }
     };
 
@@ -27,17 +27,19 @@ const CreatorsSection = () => {
         <div className={styles.cont}>
           <div className={styles.header}>
             <h2 className={styles.title}>Top Creators</h2>
-            <Link to="/create-article" className={styles.link}>
+            <Link to='/authors' className={styles.link}>
               Go to all Creators
-              {<img src={arrowIcon} alt="arrow" className={styles.icon} />}
+              {<img src={arrowIcon} alt='arrow' className={styles.icon} />}
             </Link>
           </div>
           <div className={styles.people}>
             <ul className={styles.list}>
               {creators.map(({ _id, name, avatarUrl }) => (
                 <li key={_id} className={styles.card}>
-                  <img src={avatarUrl} alt={name} className={styles.avatar} />
-                  <p className={styles.name}>{name}</p>
+                  <Link to={`/authors/${_id}`}>
+                    <img src={avatarUrl} alt={name} className={styles.avatar} />
+                    <p className={styles.name}>{name}</p>
+                  </Link>
                 </li>
               ))}
             </ul>
