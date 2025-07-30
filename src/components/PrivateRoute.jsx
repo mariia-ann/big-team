8e921848e3c40d1e26ec52c46f8da954935c1c7a
Mariia <myshyck@gmail.com>
2025-07-30 12:19:27 +0300

    fix: add settings modal

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,35 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import AuthModal from "./ModalErrorSave/ModalErrorSave.jsx";
 import { selectIsLoggedIn } from "../redux/auth/selectors.js";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  if (!isLoggedIn) {
-    return <AuthModal />
+  const [showModal, setShowModal] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      setShowModal(true);
+    }
+  }, [isLoggedIn]);
+
+  const handleClose = () => {
+    setShowModal(false);
+    if (location.key !== 'default') {
+      navigate(-1);
+    } else {
+      navigate("/");
+    }
+  };
+
+  if (!isLoggedIn && showModal) {
+    return <AuthModal onClose={handleClose} />;
   }
   return children;
-}
+};
 
-export default PrivateRoute
+export default PrivateRoute;
