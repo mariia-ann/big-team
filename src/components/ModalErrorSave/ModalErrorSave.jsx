6c1590ae65d5d017107ec6840756b23268219019
yuliia levytska <lupin773311asd@gmail.com>
2025-07-30 13:15:10 +0100

    WIP: local changes before pull

--- src/components/ModalErrorSave/ModalErrorSave.jsx
+++ src/components/ModalErrorSave/ModalErrorSave.jsx
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
 import s from "./ModalErrorSave.module.css";
 import IoClose from "../../assets/images/icons/close.svg?react";
 
 const AuthModal = ({ onClose }) => {
+  // Закриття по Escape
+  useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.key === "Escape") onClose();
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, [onClose]);
+
+  // Закриття по бекдропу
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) onClose();
+  };
+
+  // Клік по Login
+  const handleLogin = () => {
+    window.location.href = "/login";
+  };
+
+  // Клік по Register
+  const handleRegister = () => {
+    window.location.href = "/register";
+  };
+
   return (
-    <div className={s.backdrop}>
+    <div className={s.backdrop} onClick={handleBackdropClick}>
       <div className={s.modal}>
         <button className={s.closeBtn} onClick={onClose}>
           <IoClose />
@@ -13,8 +38,12 @@ const AuthModal = ({ onClose }) => {
           To save this article, you need to <br /> authorize first
         </p>
         <div className={s.buttons}>
-          <button className={s.loginBtn}>Login</button>
-          <button className={s.registerBtn}>Register</button>
+          <button className={s.loginBtn} onClick={handleLogin}>
+            Login
+          </button>
+          <button className={s.registerBtn} onClick={handleRegister}>
+            Register
+          </button>
         </div>
       </div>
     </div>
@@ -22,3 +51,4 @@ const AuthModal = ({ onClose }) => {
 };
 
 export default AuthModal;
+
