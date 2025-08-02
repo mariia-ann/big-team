74f1f00d2880e5914676e9ec962274b4207463fa
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-08-02 08:18:38 +0300

    fix: isLoggedIn  HeroSection

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
 import Container from "../Container/Container";
 import style from "./HeroSection.module.css";
 import { NavLink } from 'react-router-dom';
+import clsx from "clsx";
 
 const HeroSection = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <section className={style.section}>
       <Container>
@@ -11,16 +16,18 @@ const HeroSection = () => {
             <h1 className={style.title}>
               Find your <span className={style.fixte}>harmony</span> in community
             </h1>
-            <div className={style.buttonGroup}>
+
+            <div className={clsx(style.buttonGroup, isLoggedIn && style.alignBottom)}>
               <NavLink
                 to="/articles"
                 className={style.button}
               >
                 <p className={style.text}>Go to Articles</p>
               </NavLink>
+
               <NavLink
                 to="/register"
-                className={`${style.button} ${style.registerBtn}`}
+                className={clsx(style.button, style.registerBtn, isLoggedIn && style.hidden)}
               >
                 <p className={style.textRegister}>Register</p>
               </NavLink>
@@ -35,3 +42,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
