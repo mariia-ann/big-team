04a2582282bb660084b06fbe206bde00ed65db9d
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-08-03 12:45:52 +0200

    connecting to the backend

--- src/components/ArticlesItem2/ArticlesItem2.jsx
+++ src/components/ArticlesItem2/ArticlesItem2.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 import ButtonAddToBookmarks2 from "../ArticlesPage/ButtonAddToBookmarks2/ButtonAddToBookmarks2";
 import s from "./ArticlesItem2.module.css";
 
-const ArticlesItem2 = ({ article }) => {
-  const { _id, title, desc, img, ownerId, date } = article;
+const ArticlesItem2 = ({ article, user }) => {
+  /*if (!user) return null;*/
+  const { _id: articleId, title, desc, img, ownerId, date } = article;
+  /*const { name } = user;*/
+  const { name = "Невідомо" } = user ?? {};
 
   return (
     <div className={s.articleItem}>
@@ -14,13 +17,13 @@ const ArticlesItem2 = ({ article }) => {
       )}
       <div className={s.wrapper}>
         <div className={s.content}>
-          <p className={s.articleOwner}>{ownerId?.$oid}</p>
+          <p className={s.articleOwner}>{name}</p>
           <h3 className={s.articleTitle}>{title}</h3>
           <p className={s.articleDescription}>{desc}</p>
         </div>
         <div className={s.actions}>
-          <Link to={`/articles/${_id.$oid}`}>Learn more</Link>
-          <ButtonAddToBookmarks2 articleId={_id.$oid} />
+          <Link to={`/articles/${articleId}`}>Learn more</Link>
+          <ButtonAddToBookmarks2 articleId={articleId} />
         </div>
       </div>
     </div>
