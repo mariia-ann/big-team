3e316a997227071df507efdff723324b2f849dc5
Eone-tgx <eone1024@gmail.com>
2025-08-04 19:14:01 +0300

    add logic to article form

--- src/redux/articles/operations.js
+++ src/redux/articles/operations.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { publicAPI } from "../api/publicAPI.js";
 
-
 export const fetchArticles = createAsyncThunk(
   "articles/fetchAll",
   async (_, thunkAPI) => {
@@ -30,7 +29,16 @@ export const addArticle = createAsyncThunk(
   "articles/addArticle",
   async (item, thunkAPI) => {
     try {
-      const response = await publicAPI.get("/api/articles", item);
+      const state = thunkAPI.getState();
+      const token = state.auth.token;
+
+      if (!token) {
+        return thunkAPI.rejectWithValue("No token available");
+      }
+
+      publicAPI.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+      const response = await publicAPI.post("/api/articles", item);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
