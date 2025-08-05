bacd768d4c729d32a47096aaef0f21e9506b3a36
yuliia levytska <lupin773311asd@gmail.com>
2025-08-05 12:44:51 +0100

    fix: restore bookmarks and logic

--- src/components/ButtonAddToBookmarks/ButtonAddToBookmarks.jsx
+++ src/components/ButtonAddToBookmarks/ButtonAddToBookmarks.jsx
@@ -1,13 +1,28 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useToggle } from "../../hooks/useToggle.js";
 import s from "./ButtonAddToBookmarks.module.css";
-import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
+import { selectIsLoggedIn, selectUserId } from "../../redux/auth/selectors.js";
+import { selectBookmarks } from "../../redux/bookmarks/selectors.js";
+import { addBookmark, removeBookmark, fetchBookmarks } from "../../redux/bookmarks/operations.js";
 import AuthModal from "../ModalErrorSave/ModalErrorSave.jsx";
-import BookmarkIcon from "../../assets/images/icons/bookmark.svg?react"
+import BookmarkIcon from "../../assets/images/icons/bookmark.svg?react";
+import { useEffect } from "react";
 
 const ButtonAddToBookmarks = ({ articleId }) => {
   const { isOpen, close, open } = useToggle();
+  const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const userId = useSelector(selectUserId);
+  const bookmarks = useSelector(selectBookmarks);
+
+  
+  useEffect(() => {
+    if (isLoggedIn && userId) {
+      dispatch(fetchBookmarks(userId));
+    }
+  }, [dispatch, isLoggedIn, userId]);
+
+  const isBookmarked = bookmarks.includes(articleId);
 
   const handleClick = (e) => {
     e.stopPropagation();
@@ -15,12 +30,23 @@ const ButtonAddToBookmarks = ({ articleId }) => {
       open();
       return;
     }
-    // тут буде логіка для авторизованого користувача (додати в закладки)
+    if (!userId) return;
+
+    if (isBookmarked) {
+      dispatch(removeBookmark({ userId, articleId }));
+    } else {
+      dispatch(addBookmark({ userId, articleId }));
+    }
   };
 
   return (
     <>
-      <button onClick={handleClick} className={s.button}>
+      <button
+        onClick={handleClick}
+        className={`${s.button} ${isBookmarked ? s.active : ""}`}
+        aria-label={isBookmarked ? "Remove from bookmarks" : "Add to bookmarks"}
+        title={isBookmarked ? "Remove from bookmarks" : "Add to bookmarks"}
+      >
         <BookmarkIcon className={s.buttonIcon} size={24} />
       </button>
       {!isLoggedIn && isOpen && <AuthModal onClose={close} />}
